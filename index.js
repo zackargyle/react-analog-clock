4d587f0c03e8a1cecbc247aef08cd5879e1a8b8a
M Mahabubur Rhaman <mur-wtag@users.noreply.github.com>
2017-03-12 11:58:17 +0600

    Timezone feature added (#1)

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import AnalogClock, { Themes } from '../src/index';
 
 const WIDTH = 200;
+const GMTOFFSET= "-5.5";
 
 const Component = (
     <div>
@@ -12,6 +13,9 @@ const Component = (
         <span><AnalogClock width={WIDTH} theme={Themes.lime} /></span>
         <span><AnalogClock width={WIDTH} theme={Themes.sherbert} /></span>
         <span><AnalogClock width={WIDTH} theme={Themes.navy} /></span>
+
+        <h2>With GMT Offset -5.5:</h2>
+        <span><AnalogClock width={WIDTH} theme={Themes.light} gmtOffset={GMTOFFSET} /></span>
     </div>
 );
 
