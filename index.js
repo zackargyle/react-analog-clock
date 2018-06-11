9854e95f10568d6cabcc60951105dd51d17ef6ed
Jason Strothmann <strothj@users.noreply.github.com>
2018-06-11 13:01:18 -0400

    Add additional style settings (#6)

--- index.js
+++ index.js
@@ -3,7 +3,22 @@ import ReactDOM from 'react-dom';
 import AnalogClock, { Themes } from '../src/index';
 
 const WIDTH = 200;
-const GMTOFFSET= "-5.5";
+const GMTOFFSET = '-5.5';
+
+const customTheme = {
+    background: 'transparent',
+    border: 'transparent',
+    center: 'transparent',
+    seconds: '#000',
+    minutes: '#000',
+    hour: '#000',
+    tick: '#000',
+    smallTickWidth: 1,
+    largeTickWidth: 1,
+    secondHandWidth: 1,
+    minuteHandWidth: 1,
+    hourHandWidth: 1,
+};
 
 const Component = (
     <div>
@@ -13,6 +28,8 @@ const Component = (
         <span><AnalogClock width={WIDTH} theme={Themes.lime} /></span>
         <span><AnalogClock width={WIDTH} theme={Themes.sherbert} /></span>
         <span><AnalogClock width={WIDTH} theme={Themes.navy} /></span>
+        <span><AnalogClock width={WIDTH} theme={Themes.light} /></span>
+        <span><AnalogClock width={WIDTH} theme={customTheme} showSmallTicks={false} /></span>
 
         <h2>With GMT Offset -5.5:</h2>
         <span><AnalogClock width={WIDTH} theme={Themes.light} gmtOffset={GMTOFFSET} /></span>
